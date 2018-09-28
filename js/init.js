961a23d2a809562f8fbe1efca3ec8a2b5883f413
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-28 21:19:50 +0200

    updated more

--- js/init.js
+++ js/init.js
@@ -13,3 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {edge:"right",draggable:!0});
 });
+
+//Tooltips
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, {});
+});
