be604af99c30a18eed0876dd719c91bd4dafd6ed
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-29 08:57:14 +0200

    release v1 again forgot compiling

---
+++ build/js/init.js
@@ -0,0 +1,5 @@
+"use strict";
+//Mobile Side nav
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".sidenav");M.Sidenav.init(e,{edge:"right",draggable:!0})}),//Tooltips
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".tooltipped");M.Tooltip.init(e,{})});
+//# sourceMappingURL=init.js.map
