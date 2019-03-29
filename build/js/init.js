c3e2f3e44a8b9ffe97522851d47a9155e225276f
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-29 20:43:01 +0100

    Added analytics

--- build/js/init.js
+++ build/js/init.js
@@ -1,5 +1,1 @@
-"use strict";
-//Mobile Side nav
-document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".sidenav");M.Sidenav.init(e,{edge:"right",draggable:!0})}),//Tooltips
-document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".tooltipped");M.Tooltip.init(e,{})});
-//# sourceMappingURL=init.js.map
+"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".sidenav");M.Sidenav.init(e,{edge:"right",draggable:!0})}),document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".tooltipped");M.Tooltip.init(e,{})});
