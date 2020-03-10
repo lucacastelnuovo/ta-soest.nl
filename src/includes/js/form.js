189727199b0b187bb141fb84744074fea90ac919
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-03-10 20:19:28 +0100

    Performance Improvement (#10)
    * initial transfer to new system
    
    * stage 2
    
    * stage 3
    
    * stage 4
    
    * reverse article order
    
    * stage 6
    
    * add 404, add meta
    
    * start csp and include form js
    
    * change name
    
    * fix #13
    
    * fix #
    
    * larger button
    
    * only load form listener if form is present
    
    * disable csp for testing purposes
    
    * csp done
    
    * add preconnect
    
    * show pdf direct
    
    * fix #11

--- src/includes/js/form.js
+++ src/includes/js/form.js
@@ -10,41 +10,43 @@ function buttonsState(active, form) {
     }
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    console.log("formData", formData);
-
-    if (!validEmail(formData.get("email"))) {
-        document.querySelector("#email_invalid").style.display = "block";
-        return false;
-    }
-
-    document.querySelector("#email_invalid").style.display = "none";
-    buttonsState(false, form);
-
-    fetch("/", {
-        method: "POST",
-        headers: {
-            Accept: "application/x-www-form-urlencoded;charset=UTF-8",
-            "Content-Type":
-                "application/x-www-form-urlencoded;charset=UTF-8"
-        },
-        body: new URLSearchParams(formData).toString()
-    })
-        .then(res => {
-            if (res.status !== 200) {
-                throw new Error("Not 200 response");
-            }
-
-            form.style.display = "none";
-            document.querySelector("#thankyou_message").style.display =
-                "block";
+const form = document.querySelector("form");
+if (form) {
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        console.log("formData", formData);
+    
+        if (!validEmail(formData.get("email"))) {
+            document.querySelector("#email_invalid").style.display = "block";
+            return false;
+        }
+    
+        document.querySelector("#email_invalid").style.display = "none";
+        buttonsState(false, form);
+    
+        fetch("/", {
+            method: "POST",
+            headers: {
+                Accept: "application/x-www-form-urlencoded;charset=UTF-8",
+                "Content-Type":
+                    "application/x-www-form-urlencoded;charset=UTF-8"
+            },
+            body: new URLSearchParams(formData).toString()
         })
-        .catch(() => {
-            document.querySelector("#submission_invalid").style.display =
-                "block";
-            buttonsState(true, form);
-        });
-});
+            .then(res => {
+                if (res.status !== 200) {
+                    throw new Error("Not 200 response");
+                }
+    
+                form.style.display = "none";
+                document.querySelector("#thankyou_message").style.display =
+                    "block";
+            })
+            .catch(() => {
+                document.querySelector("#submission_invalid").style.display =
+                    "block";
+                buttonsState(true, form);
+            });
+    });
+}
